458d0fa25ae0d368ac87ed72d7c5120cd9cfdcf7
Saitejakommoju9 <saitejakommoju71@gmail.com>
2026-08-04 07:27:48 +0530

    added responsive

--- src/componenets/Favourites.jsx
+++ src/componenets/Favourites.jsx
@@ -12,7 +12,7 @@ function Favourites(){
     }
     return(
         <div>
-            <div className="absolute right-0 top-1 sm:right-30 my-4">
+            <div className="absolute right-5 top-1 sm:right-30 my-4">
                 <button className="bg-green-500 py-2 px-4 cursor-pointer  text-white rounded-lg " onClick={handleClick}>Home Page</button>
             </div>
             <h1 className="text-red-500 left-1/2 my-18 text-center -ml-11 sm:-ml-10 text-3xl sm:text-5xl absolute font-bold">Favourites!</h1>
@@ -26,7 +26,7 @@ function Favourites(){
                     <div className="mt-34 z-10 flex  mx-3">
                     <MoviesCard movie={movie} poster={movie?.poster_path}/>
                     </div>
-                )) : <h1 className="font-medium text-3xl absolute my-30 text-gray-300 text-center left-1/2 -ml-40 ">No Movie Are In Favourites!!</h1>
+                )) : <h1 className="font-medium text-3xl absolute my-30 text-gray-300 text-center left-1/2 -ml-40 ">    Add Movies to favourite!!</h1>
             }
             </div>
 
